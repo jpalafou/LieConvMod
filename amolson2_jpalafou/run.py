f17324f957e0c072bcbae13b192b4fdefbd32c22
Jonathan Palafoutas <jpalafou@princeton.edu>
2023-05-07 23:46:40 -0400

    arg parse for training with different groups

--- amolson2_jpalafou/run.py
+++ amolson2_jpalafou/run.py
@@ -24,60 +24,51 @@ Trivialmodel = HLieResNet(k = k, num_layers = num_layers, center = center, group
 Trivialmodel.load_state_dict(
     torch.load(f"models/springmodel_Trivial.pt"
     )['model_state'])
-SO2model = HLieResNet(k = k, num_layers = num_layers, center = center, group=SO2())
-SO2model.load_state_dict(
-    torch.load(f"models/springmodel_SO2.pt"
-    )['model_state'])
 Tmodel = HLieResNet(k = k, num_layers = num_layers, center = center, group=T(2))
 Tmodel.load_state_dict(
     torch.load(f"models/springmodel_T.pt"
+    )['model_state'])
+SO2model = HLieResNet(k = k, num_layers = num_layers, center = center, group=SO2())
+SO2model.load_state_dict(
+    torch.load(f"models/springmodel_SO2.pt"
     )['model_state'])
 print('Loaded models')
 
 # run simulation using model dynamics
 dataidx = 0
 bs = 10
-T = 200
+T = 100
 
 z0 = z[:, 0, :]
 zs_Trivial = model_integrator(Trivialmodel, t, z0, sysp, batch_size=bs, T=T)
-zs_SO2 = model_integrator(SO2model, t, z0, sysp, batch_size=bs, T=T)
 zs_T = model_integrator(Tmodel, t, z0, sysp, batch_size=bs, T=T)
+zs_SO2 = model_integrator(SO2model, t, z0, sysp, batch_size=bs, T=T)
 print('Ran spring simulation using neural net dynamics')
 
 # plot simulation momentums to verify that they remain constant
 px, py, pang = evaluate_2d_momentum(z)
 px_pred_Trivial, py_pred_Trivial, pang_pred_Trivial = evaluate_2d_momentum(zs_Trivial)
-px_pred_SO2, py_pred_SO2, pang_pred_SO2 = evaluate_2d_momentum(zs_SO2)
 px_pred_T, py_pred_T, pang_pred_T = evaluate_2d_momentum(zs_T)
+px_pred_SO2, py_pred_SO2, pang_pred_SO2 = evaluate_2d_momentum(zs_SO2)
 
-fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(8, 10))
+fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8))
 
-# x momentum
-ax1.plot(t[dataidx, 0:T], px[dataidx, 0:T], label='dataset')
-# ax1.plot(t[dataidx, 0:T], px_pred_Trivial[dataidx].detach().numpy(), '--', label='Trivial predicted')
-ax1.plot(t[dataidx, 0:T], px_pred_SO2[dataidx].detach().numpy(), '--', label='SO2 predicted')
-ax1.plot(t[dataidx, 0:T], px_pred_T[dataidx].detach().numpy(), '--', label='T2 predicted')
-ax1.set_xlabel('t')
-ax1.set_ylabel('linear momentum in x')
+# linear momentum
+ax1.plot(t[dataidx, 0:T], px[dataidx, 0:T], label=r'dataset $p_x$')
+ax1.plot(t[dataidx, 0:T], py[dataidx, 0:T], label=r'dataset $p_y$')
+ax1.plot(t[dataidx, 0:T], px_pred_Trivial[dataidx].detach().numpy(), '--', label=r'Trivial predicted $p_x$')
+ax1.plot(t[dataidx, 0:T], py_pred_Trivial[dataidx].detach().numpy(), '--', label=r'Trivial predicted $p_y$')
+ax1.plot(t[dataidx, 0:T], px_pred_T[dataidx].detach().numpy(), '--', label=r'T2 predicted $p_x$')
+ax1.plot(t[dataidx, 0:T], py_pred_T[dataidx].detach().numpy(), '--', label=r'T2 predicted $p_y$')
+ax1.plot(t[dataidx, 0:T], px_pred_SO2[dataidx].detach().numpy(), '--', label=r'SO2 predicted $p_x$')
+ax1.plot(t[dataidx, 0:T], py_pred_SO2[dataidx].detach().numpy(), '--', label=r'SO2 predicted $p_y$')
 ax1.legend()
 
-# y momentum
-ax2.plot(t[dataidx, 0:T], py[dataidx, 0:T], label='dataset')
-# ax2.plot(t[dataidx, 0:T], py_pred_Trivial[dataidx].detach().numpy(), '--', label='Trivial predicted')
-ax2.plot(t[dataidx, 0:T], py_pred_SO2[dataidx].detach().numpy(), '--', label='SO2 predicted')
-ax2.plot(t[dataidx, 0:T], py_pred_T[dataidx].detach().numpy(), '--', label='T2 predicted')
+# angular momentum
+ax2.plot(t[dataidx, 0:T], pang[dataidx, 0:T], label='dataset')
+ax2.plot(t[dataidx, 0:T], pang_pred_T[dataidx].detach().numpy(), '--', label=r'T2 predicted $p_{\theta}$')
+ax2.plot(t[dataidx, 0:T], pang_pred_SO2[dataidx].detach().numpy(), '--', label=r'SO2 predicted $p_{\theta}$')
 ax2.set_xlabel('t')
-ax2.set_ylabel('linear momentum in y')
 ax2.legend()
 
-# angular momentum
-ax3.plot(t[dataidx, 0:T], pang[dataidx, 0:T], label='dataset')
-# ax3.plot(t[dataidx, 0:T], pang_pred_Trivial[dataidx].detach().numpy(), '--', label='Trivial predicted')
-ax3.plot(t[dataidx, 0:T], pang_pred_SO2[dataidx].detach().numpy(), '--', label='SO2 predicted')
-ax3.plot(t[dataidx, 0:T], pang_pred_T[dataidx].detach().numpy(), '--', label='T2 predicted')
-ax3.set_xlabel('t')
-ax3.set_ylabel('angular momentum')
-ax3.legend()
-
-plt.show()
+plt.savefig('figure.png', dpi=300)
--- amolson2_jpalafou/train_springs_HLieResNet.py
+++ amolson2_jpalafou/train_springs_HLieResNet.py
@@ -1,10 +1,25 @@
+import argparse
 import torch
 from lie_conv.dynamicsTrainer import HLieResNet
 from lie_conv.lieGroups import Trivial, T, SO2
 from spring_trainer import makeTrainer
 from model_config import num_layers, k, center, num_epochs, n_train
 
-group = T(2)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--group',
+    default=Trivial(2), 
+    type=str, 
+    choices = ['Trivial', 'T2', 'SO2']
+    )
+args = parser.parse_args()
+
+if args.group == 'Trivial':
+    group = Trivial(2)
+if args.group == 'T2':
+    group = T(2)
+if args.group == 'SO2':
+    group = SO2(0)
 
 trainer = makeTrainer(
     num_epochs = num_epochs,
